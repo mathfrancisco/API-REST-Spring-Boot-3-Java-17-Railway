f3dee2c1ae80540ce232d7ebc0ec8dfcebe206f4
mathfrancisco2 <math.francisco2@gmail.com>
2024-08-13 21:16:04 -0300

    projeto.att

---
+++ desafioapiRest/desafioapiRest/src/main/java/apiRest/desafioapiRest/DesafioapiRestApplication.java
@@ -0,0 +1,13 @@
+package desafioapiRest.src.main.java.apiRest.desafioapiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioapiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioapiRestApplication.class, args);
+	}
+
+}
---
+++ desafioapiRest/desafioapiRest/src/test/java/apiRest/desafioapiRest/DesafioapiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package apiRest.desafioapiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioapiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
